fd06e0bad26fa680ea1f07f2e9be119f706744f7
Malico Yong <malico.yong@gmail.com>
2019-03-08 01:10:26 +0100

    Configurations

--- config/momo.php
+++ config/momo.php
@@ -7,5 +7,14 @@ return [
      */
     'email' => env('MOMO_EMAIL'),
 
+    /**
+     * Default Price to all the Site Transactions
+     */
+    'default_price' => env('MOMO_DEFAULT_PRICE', 100),
 
+    /**
+     * To include the Momo\Support\MomoTransaction trait to your model that creates relationship between your model and Momo\Model\Transaction Model.
+     * Here you specify the foreign_key as it is found on your model migration.
+     */
+    'foreign_key' => "momo_transaction_id"
 ];
