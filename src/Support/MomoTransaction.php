664f16a9f3240fc6b69f8770628921ed5d65fe0f
Malico <hi@malico.me>
2019-10-02 18:10:52 +0100

    Methods to access MomoBuillder

--- src/Support/MomoTransaction.php
+++ src/Support/MomoTransaction.php
@@ -2,10 +2,37 @@
 
 namespace  Malico\Momo\Support;
 
+use Malico\Momo\Momo;
+
 trait MomoTransaction
 {
+    /**
+     * relationshp to Momo Transaction
+     */
     public function momo_transaction()
     {
-        return $this->hasOne('Malico\Momo\Model\Transaction', 'id', config('momo.foreign_key'));
+        return $this->belongsTo('Malico\Momo\Model\Transaction', 'id', config('momo.foreign_key'));
+    }
+
+    /**
+     * Return MOMO Builder
+     * @param  STring|int|null $tel
+     * @param  Int|NUll $amount
+     * @return Malico\Momo\Support\MomoBuilder
+     */
+    public function momo($tel = null, $amount = null)
+    {
+        return (new MomoBuilder($this, $tel, $amount));
+    }
+
+    /**
+     * Make Payment
+     * @param  String|Int|Null $tel
+     * @param  Int|Null $amount
+     * @return void
+     */
+    public function pay($tel = null, $amount = null)
+    {
+        return  $this->momo($tel, $amount)->pay();
     }
 }
